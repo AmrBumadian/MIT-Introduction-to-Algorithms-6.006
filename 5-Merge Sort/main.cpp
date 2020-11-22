bfafc1e45cb8ef1466e3f874fbc366d65b3f60a8
AmrBumadian <35141856+AmrBumadian@users.noreply.github.com>
2020-11-22 06:47:22 +0200

    count inversions while merge sorting

--- 5-Merge Sort/main.cpp
+++ 5-Merge Sort/main.cpp
@@ -21,6 +21,8 @@ inline void BOOST() {
     cin.tie(0), cout.tie(0), cin.sync_with_stdio(0), cout.sync_with_stdio(0);
 }
 
+int inversionCount = 0;
+
 void merge(vi &a, int p, int q, int r) { // merge the two sorted partitions back to A
     int n1 = q - p + 1;
     int n2 = r - q;
@@ -38,6 +40,8 @@ void merge(vi &a, int p, int q, int r) { // merge the two sorted partitions back
             a[k] = left[i];
             ++i; // advance pointer to the next element in left
         } else {
+            // since right[j] is smaller than left[i], it makes an inversion with all elements left[i..n1]
+            inversionCount += (n1 - i);
             a[k] = right[j];
             ++j; // advance pointer to the next element in right
         }
@@ -45,21 +49,19 @@ void merge(vi &a, int p, int q, int r) { // merge the two sorted partitions back
     if (i < n1) { // if left wasn't fully copied
         while (k <= r) { // copy the rest of left back to A
             a[k] = left[i];
-            ++i;
-            ++k;
+            ++i, ++k;
         }
     } else {
         while (k <= r) { // copy the rest of right back to A
             a[k] = right[j];
-            ++j;
-            ++k;
+            ++j, ++k;
         }
     }
 }
 
 void mergeSort(vi &arr, int p, int r) {
     if (p < r) { // if the array has more than 1 element
-        int q = (p + r - 1) / 2; // partitioning around q
+        int q = (p + r) / 2; // partitioning around q
         mergeSort(arr, p, q); // sort the left partition
         mergeSort(arr, q + 1, r); // sort the right partition
         merge(arr, p, q, r); // merge the two partitions
@@ -73,7 +75,8 @@ int main() {
     for (int i = 0; i < n; ++i) {
         cin >> a[i];
     }
-    mergeSort(a, 0, n);
+    mergeSort(a, 0, n - 1);
     for (int i = 0; i < n; ++i) cout << a[i] << " ";
     cout << el;
+    cout << inversionCount;
 }
